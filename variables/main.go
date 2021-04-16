4a5293515b3770e23793e1dc9c3c0b596e2958bf
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-04-16 16:05:00 +0530

    Shorthand method for variable declaration

--- variables/main.go
+++ variables/main.go
@@ -5,11 +5,18 @@ import "fmt"
 func main() {
 
 	// using var
-	var name = "Thor"
-	var age = 30
+	// var name = "Thor"
+	var age int32 = 30
+	const isGood = true
+	var size float32 = 3.4
 
-	fmt.Println(name, age)
+	// Shorthand method
+	name := "Thor"
+	length := 2.9
 
+	email,username := "thor@gmail.com","THORR"
+	
+	fmt.Println(name, age, isGood,size,email,username)
 	// get variable data type
-	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", length)
 }
