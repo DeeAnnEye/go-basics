de4485c0f6dc29037e11982861a1d5fcb67ec30c
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-04-16 15:53:34 +0530

    Added folder variables

---
+++ variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// using var
+	var name = "Thor"
+	var age = 30
+
+	fmt.Println(name, age)
+
+	// get variable data type
+	fmt.Printf("%T\n", age)
+}
