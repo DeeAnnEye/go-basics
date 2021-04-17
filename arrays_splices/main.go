62c1510c89a5a1208681b647bbef546aef98c60b
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-04-17 14:36:57 +0530

    Modified arrays_splices

--- arrays_splices/main.go
+++ arrays_splices/main.go
@@ -10,5 +10,10 @@ func main(){
 	fruitArr[0] = "Apple"
 	fruitArr[1] = "Orange"
 
+	// declare and assign array
+	names := [2]string{"Mark","Ben"}
+
 	fmt.Println(fruitArr)
+	fmt.Println(fruitArr[0])
+	fmt.Println(names)
 }
